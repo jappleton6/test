7e093cabfd72568127a26b0f41ea83877c8057bd
rrichard-gcps <rrichard-gcps@users.noreply.github.com>
2015-01-07 09:08:04 -0500

    Create sample1.r

---
+++ sample1.r
@@ -0,0 +1,14 @@
+#R Data Analysis Examples: Robust Regression
+require(foreign)
+require(MASS)
+
+#Description of the example data----
+cdata <- read.dta("http://www.ats.ucla.edu/stat/data/crime.dta")
+summary(cdata)
+
+#Using robust regression analysis----
+summary(ols <- lm(crime ~ poverty + single, data = cdata))
+
+#plot results----
+opar <- par(mfrow = c(2,2), oma = c(0, 0, 1.1, 0))
+plot(ols, las = 1)
